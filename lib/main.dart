68b6f711c8a0653d862317cc922b320c60b8a795
Arkin <75495436+arkinator1000@users.noreply.github.com>
2022-06-19 01:56:51 -0700

    Add settings needed for Auth0

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,15 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'homepage.dart';
 
+final FlutterAppAuth appAuth = FlutterAppAuth();
+const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
+const String AUTH0_ISSUER = 'dev-bpy22x96.us.auth0.com';
+const String AUTH0_DOMAIN = 'https://$AUTH0_ISSUER';
+const String AUTH0_CLIENT_ID = 'ryyH61QkgKI5KEoB4XA9SR7OlgBFAh6b';
+// May not need this:
+// const String AUTH0_REDIRECT_URI = 'com.{your domain}.{your app}://login-callback';
+
 void main() {
   runApp(const MyApp());
 }
