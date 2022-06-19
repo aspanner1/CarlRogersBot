69672eae3c7b6eb92ccca0fd5e326aa8a7349734
Arkin <75495436+arkinator1000@users.noreply.github.com>
2022-06-19 02:37:47 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 
-// Imports for Auth0
+/* Future Updates: Imports for Auth0
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+*/
 
 import 'homepage.dart';
-
-final FlutterAppAuth appAuth = FlutterAppAuth();
-const FlutterSecureStorage secureStorage = FlutterSecureStorage();
-
-const String AUTH0_ISSUER = 'dev-bpy22x96.us.auth0.com';
-const String AUTH0_DOMAIN = 'https://$AUTH0_ISSUER';
-const String AUTH0_CLIENT_ID = 'ryyH61QkgKI5KEoB4XA9SR7OlgBFAh6b';
-// May not need this:
-// const String AUTH0_REDIRECT_URI = 'com.{your domain}.{your app}://login-callback';
 
 void main() {
   runApp(const MyApp());
