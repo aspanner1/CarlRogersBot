3fa3bac2bd4e4e5ebc5fb0ef33999aded0103355
Arkin <75495436+arkinator1000@users.noreply.github.com>
2022-06-19 01:31:18 -0700

    Added additional imports for Auth0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+// Imports for Auth0
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'homepage.dart';
 
 void main() {
